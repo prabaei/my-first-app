d0387e69048d750d1ae5ecf4233df7254038bfe0
prabaei <prabaei@gmail.com>
2019-11-05 17:03:47 +0530

    first section over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessmessageComponent } from './successmessage/successmessage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessmessageComponent
   ],
   imports: [
     BrowserModule,
